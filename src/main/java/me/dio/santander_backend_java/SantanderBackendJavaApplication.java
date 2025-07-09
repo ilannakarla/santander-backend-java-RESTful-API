7bbabbbbf37fdeb849693ca87e4f29bc8b63679a
ilannakarla <ilannakarla@hotmail.com>
2025-07-08 23:59:27 -0300

    Initial commit

---
+++ src/main/java/me/dio/santander_backend_java/SantanderBackendJavaApplication.java
@@ -0,0 +1,13 @@
+package me.dio.santander_backend_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderBackendJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderBackendJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/santander_backend_java/SantanderBackendJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.santander_backend_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderBackendJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
